6afc218c1b9289129c9273b2bfba70ac0cb8e5a6
Khoa Tran <jacktran1799@gmaail.com>
2020-02-07 23:00:08 -0600

    version 1.0, initial set up

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [name,setName]= useState();
+  const [url, setUrl]= useState('https://starwars-visualguide.com/assets/img/characters/1.jpg');
+  const [gender,setGender]=useState();
+
+  useEffect(()=>{
+    axios
+    .get('https://swapi.co/api/people/')
+    .then(res=>{
+      console.log(res);
+      setName(res.data.results[0].name);
+      setGender(res.data.results[0].gender)})
+    .catch()
+  },[]);
+
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -11,7 +24,10 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <h1 className="Header">Star Wars: Characters Guide</h1>
+      <h1> {name} </h1> 
+      <img  src={url}  alt='just an img'/>
+      <h2> {gender} </h2>
     </div>
   );
 }
